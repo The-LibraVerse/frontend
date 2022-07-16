ecfbd5517db3a1f42b745acd3cca44de634183f9
Adesuwa T. Dawodu <littlezigy@gmail.com>
2022-07-16 01:50:05 +0100

    Refactoring: code organisation

--- concat-css.sh
+++ concat-css.sh
@@ -1,8 +1,17 @@
 cat  \
-styles/general.css \
-styles/book-cover/index.css \
-styles/book-cover/home-page.book-cover.css \
-styles/book.page.css \
-styles/home.css \
-styles/reader.css \
-> styles/min.css
+        styles/site-variables.colors.css \
+        styles/common.blocks/general.css \
+        styles/common.blocks/button/button.css \
+        styles/common.blocks/form/form.css \
+        styles/common.blocks/input-group/input-group.css \
+        styles/common.blocks/input-group/input-group_minimalist.css \
+        styles/common.blocks/text-editor/text-editor.css \
+        styles/common.blocks/image-upload/image-upload.css \
+        styles/common.blocks/book-cover/book-cover.css \
+        styles/common.blocks/book-cover/book-cover_list-item.css \
+        styles/common.blocks/book-cover/book-cover_thumbnail.css \
+        styles/common.blocks/book-cover/home-page.book-cover.css \
+        styles/other.css \
+        styles/book.page.css \
+        styles/reader.css \
+        > styles/min.css
