a968a902509d97b3213ce4748b6a14bd1c043cd1
Adesuwa T. Dawodu <littlezigy@gmail.com>
2022-07-21 10:54:53 +0100

    Error handling for toast

--- src/toastFunctions.js
+++ src/toastFunctions.js
@@ -1,26 +1,30 @@
 export function hideToast() {
     const toast = document.getElementById('site-toast')
 
-    toast.style.opacity = 0;
-    setTimeout(() => toast.style.display='none', 1000);
+    if(toast) {
+        toast.style.opacity = 0;
+        setTimeout(() => toast.style.display='none', 1000);
+    }
 }
 
 export function showToast(content, type='error', hideToast=true) {
     const toast = document.getElementById('site-toast');
     console.log('toast:', toast);
 
-    switch(type) {
-        case 'error':
-        default:
-            toast.classList.add('notice_error');
-            break;
+    if(toast) {
+        switch(type) {
+            case 'error':
+            default:
+                toast.classList.add('notice_error');
+                break;
+        }
+
+        toast.innerHTML= "<p class='notice__message'>" + content + "</p>";
+
+        toast.style.display = 'block';
+        toast.style.opacity = 1;
+
+        if(hideToast === true)
+            setTimeout(hideToast, 5000);
     }
-
-    toast.innerHTML= "<p class='notice__message'>" + content + "</p>";
-
-    toast.style.display = 'block';
-    toast.style.opacity = 1;
-
-    if(hideToast === true)
-        setTimeout(hideToast, 5000);
 }
